a2a0fdae8e2b1e8444aa36847e13fa074324bf62
Alex Falke <afalkear@gmail.com>
2015-10-30 08:52:53 -0300

    [#107025850] devise installation

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
