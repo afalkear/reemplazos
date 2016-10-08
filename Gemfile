b8da992d5f9fab65a58a1c164ac1c55629851502
Alex Falke <afalkear@gmail.com>
2016-10-08 16:04:20 -0300

    [#131512481] style and hours and days local variable

--- Gemfile
+++ Gemfile
@@ -46,4 +46,3 @@ gem 'puma'
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
-
--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,9 +3,21 @@ class ActivitiesController < ApplicationController
 
   def index
     @activities = Activity.where(user_id: current_user.id)
+    @hours = default_hours
+    @days = default_days
   end
 
   def create
     @activity = Activity.new
+  end
+
+  private
+
+  def default_hours
+    (8..20).to_a
+  end
+
+  def default_days
+    #current_month.next_5_days
   end
 end
