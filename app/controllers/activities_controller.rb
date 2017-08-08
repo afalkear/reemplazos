5f4da35ba913148229f9436dcca83e49764a227c
Alex Falke <afalkear@gmail.com>
2017-08-08 12:49:37 -0300

    [#150047750] add ability to destroy activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -11,12 +11,27 @@ class ActivitiesController < ApplicationController
     current_user.activities << Activity.new(activity_params)
 
     if current_user.activities.last.save
-      flash[:notice] = "Activity succesfully created"
+      flash[:notice] = "Activity successfully created"
     else
       flash[:alert] = "Activity could not be created due to: #{current_user.activities.last.errors.full_messages}"
     end
 
     redirect_to activities_path
+  end
+
+  def destroy
+    activity = Activity.find(params[:id])
+    
+    if activity.destroy
+      flash[:notice] = "Activiy successfully destroyed"
+    else
+      flash[:alert] = "Activity could not be destroyed"
+    end
+    
+    respond_to do |format|
+      format.json { render json: "destroyed" }
+      format.html { redirect_to activities_path }
+    end
   end
 
   private
