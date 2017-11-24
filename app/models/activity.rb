06def2d3c7f5dc267a74ac8d1b8072b35b0ac958
Alex Falke <afalkear@gmail.com>
2017-11-24 20:05:45 -0300

    [#150388300] set default alarms

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -7,6 +7,7 @@ class Activity < ApplicationRecord
 
   validates :name, :date, :start_hour, :end_hour, :responsible_id, presence: true
   validates_inclusion_of :start_hour, in: 0..24
+  after_create set_default_alarms
 
   def start_at
     "#{start_hour}:#{start_minutes}"
@@ -14,5 +15,13 @@ class Activity < ApplicationRecord
 
   def end_at
     "#{end_hour}:#{end_minutes}"
+  end
+
+  def set_default_alarms
+    self.alarms.create(
+      offset: 1, 
+      offset_type: "day", 
+      user_id: user.id, 
+      responsible_id: responsible.id)
   end
 end
